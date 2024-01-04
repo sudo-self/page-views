458b72eeaf4f2c342f38260f1fbea9b786b5a182
Sudo-Self <sudo-self@proton.me>
2024-01-04 02:05:56 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,28 +9,27 @@
   class="relative flex flex-col items-center justify-center min-h-screen py-10"
 >
   <a
-    href="https://vercel.com"
+    href="https://jessejesse.com"
     class="px-6 py-2 text-sm font-medium text-gray-600 transition-all rounded-full shadow-sm bg-white/30 dark:bg-white/80 ring-1 ring-gray-900/5 dark:text-black hover:shadow-lg active:shadow-sm"
   >
-    Deploy your own to Vercel
+   ENTER
   </a>
   <h1
     class="pt-4 pb-8 bg-gradient-to-br dark:from-white from-black via-[#707070] to-[#ffffff] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
   >
-    Vercel KV
+   JesseJesse.com
   </h1>
   <div
     class="flex flex-col items-center w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"
   >
     <div class="mb-6 text-lg text-center">
-      Displayed below is the total number of page views for this demo. Refresh
-      the page and watch it increase!
+     KV datastore
     </div>
     <button
       on:click={refreshPage}
       class="mb-6 font-bold underline opacity-70 hover:opacity-100"
     >
-      Refresh Page
+     Visitors!
     </button>
     <div class="text-5xl font-bold">
       {data?.pageVisits}
@@ -39,17 +38,17 @@
   <div
     class="w-full max-w-lg mt-6 font-light text-center text-gray-600 dark:text-gray-300"
   >
-    Simple hello world demo of
+   Svelte Web-App
     <a
-      href="https://vercel.com/kv"
+      href="https://svelte-webapp-tsw7.vercel.app/about"
       class="font-medium underline transition-colors underline-offset-4 dark:hover:text-white hover:text-black"
     >
-      Vercel KV
+     Github
     </a>
     <div class="flex items-center justify-center my-2">
-      <span>Built with</span>
+      <span>built by svelte</span>
       <a
-        href="https://kit.svelte.dev/"
+        href="https://svelte-webapp-tsw7.vercel.app"
         class="flex items-center font-medium underline transition-colors underline-offset-4 dark:hover:text-white hover:text-black"
       >
         <img src="svelte_logo.png" alt="svelte logo" class="h-6 mx-2" />
@@ -69,7 +68,7 @@
       />
     </a>
     <a
-      href="https://github.com/vercel/examples/tree/main/storage/kv-redis-sveltekit"
+      href="https://github.com/sudo-self/svelte"
       class="flex items-center h-8 mt-auto space-x-2 bottom-20 right-20"
     >
       <img src="/github.svg" alt="GitHub Logo" class="h-6 dark:invert" />
