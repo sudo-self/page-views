afc8f838c90fbdc0d8928854502b51a48cbf4c09
Sudo-Self <sudo-self@proton.me>
2024-01-06 02:22:48 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,7 +39,7 @@
   >
  
     <div class="flex items-center justify-center my-2">
-      <span><a href="https://openai-ufo.vercel.app"><pre>OpenAi + Svelte&nbsp;<img src="/github.svg" alt="GitHub Logo" class="h-6 dark:invert" />&nbsp;Vercel + Redis</pre></a></span>
+      <span><a href="https://openai-ufo.vercel.app"><pre>OpenAi + Svelte&nbsp;&nbsp;&nbsp;&nbsp;<img src="/github.svg" alt="GitHub Logo" class="h-6 dark:invert" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Vercel + Redis</pre></a></span>
   </div>
   <div class="flex flex-col grow">
 
